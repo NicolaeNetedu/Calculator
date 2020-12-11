eb8b0b894deb34adfd2fa1a0b52043b0cacfefc2
NNN <nicolaenmd@gmail.com>
2020-12-11 17:19:50 +0200

    added inform in index, style, script

--- calcJa.js
+++ calcJa.js
@@ -1,51 +1,69 @@
-let symbols = document.querySelectorAll('.symbols')
-const screenCalculator = document.querySelector('#screenCalc')
-let butoane = document.querySelectorAll('.butoane')
-let ravno = document.getElementById('ravno')
+function insert(num) {
+ document.calculator.textviz.value = document.calculator.textviz.value + num
+};
 
-let firstNumber = []
-let secondNumber = []
-console.log(firstNumber)
-console.log(secondNumber)
-console.log(symbols)
-let x = [symbols]
-console.log(x)
+function egal() {
+    let exp = document.calculator.textviz.value;
+    if (exp) {
+        document.calculator.textviz.value = eval(exp);
+    }
+};
 
-butoane.forEach((button)=> {
-    button.addEventListener('click', () => {
-        screenCalculator.append(button.value)
-        firstNumber = Number(button.value)
+function sterge() {
 
+    document.calculator.textviz.value = "";
+
+}
 
 
-    })
-})
-
-symbols.forEach((el)=> {
-    el.addEventListener('click', () => {
-        screenCalculator.append(el.value)
-        if (el.value === '+'){
-            butoane.forEach((button)=> {
-                button.addEventListener('click', () => {
-                    secondNumber=Number(button.value)
-
-
-
-
-                })
-
-            })
-        }
-
-    })
-})
-
-ravno.addEventListener('click',()=>{
-    screenCalculator.append(ravno.value)
-   screenCalculator.append(secondNumber+firstNumber)
-
-
-})
+// let symbols = document.querySelectorAll('.symbols')
+// const screenCalculator = document.querySelector('#screenCalc')
+// let butoane = document.querySelectorAll('.butoane')
+// let ravno = document.getElementById('ravno')
+//
+// let firstNumber = []
+// let secondNumber = []
+// console.log(firstNumber)
+// console.log(secondNumber)
+// console.log(symbols)
+// let x = [symbols]
+// console.log(x)
+//
+// butoane.forEach((button)=> {
+//     button.addEventListener('click', () => {
+//         screenCalculator.append(button.value)
+//         firstNumber = Number(button.value)
+//
+//
+//
+//     })
+// })
+//
+// symbols.forEach((el)=> {
+//     el.addEventListener('click', () => {
+//         screenCalculator.append(el.value)
+//         if (el.value === '+'){
+//             butoane.forEach((button)=> {
+//                 button.addEventListener('click', () => {
+//                     secondNumber=Number(button.value)
+//
+//
+//
+//
+//                 })
+//
+//             })
+//         }
+//
+//     })
+// })
+//
+// ravno.addEventListener('click',()=>{
+//     screenCalculator.append(ravno.value)
+//    screenCalculator.append(secondNumber+firstNumber)
+//
+//
+// })
 
 // function allCalcFunct(a,operator,b){
 //
